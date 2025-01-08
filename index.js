b63af84365acfa391a7eae5797798fe1d498a5af
CrispysCode <pr0ject0dinx@gmail.com>
2025-01-07 22:44:55 -0600

    Referenced all our HTML elements in JS for functionality.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let num;
-let operator
-let anotherNum;
+const btns = document.querySelectorAll(".btn-num, .btn-oper");
+const display = document.querySelector(".display");
+const equalBtn = document.querySelector(".btn-equal")
+const clearBtn = document.querySelector(".btn-clear");
+let currentInput = "";
+let prevInput = "";
+let oper = "";
+
+clearBtn.addEventListener('click', () => {
+    display.textContent = "";
+});
 
 function add(a, b) {
   return a + b;
@@ -15,7 +23,7 @@ const multiply = (a, b) => {
 }
 
 const divide = (a, b) => {
-  return a / b;
+  return b !== 0 ? a / b : "Error";
 }
 
 function operate(operator, a, b) {
@@ -33,3 +41,10 @@ function operate(operator, a, b) {
     }
 }
 
+btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const btnValue = btn.getAttribute('data-num');
+        if (btn.classList.contains('btn-num')) {}
+        display.textContent += btn.textContent;
+    })
+})
